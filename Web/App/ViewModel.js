9bbad005d4f12e20ec0604749af2a1362f76f901
Darcy Buttrose <darcy.buttrose@gmail.com>
2014-07-26 20:13:01 +0800

    DB - added template

---
+++ Web/App/ViewModel.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    function Person(name,isComputer,element) {
+        var self = this;
+
+        self.name = ko.observable(name);
+        self.isComputer = ko.observable(isComputer);
+        self.element = ko.observable(element);
+    }
+
+    function Viewmodel() {
+        var self = this;
+
+        self.player1 = ko.observable(new Person("Person", false, "Rock"));
+        self.player2 = ko.observable(new Person("Computer", true, "Paper"));
+    };
+    
+    $(document).ready(function() {
+        ko.applyBindings(new Viewmodel());
+    });
+})();
