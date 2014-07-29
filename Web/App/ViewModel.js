bea8c1b6f0cb6f13ca17e9a23eacb8a30982877c
Darcy Buttrose <darcy.buttrose@gmail.com>
2014-07-29 20:40:20 +0800

    DB - added feedback

--- Web/App/ViewModel.js
+++ Web/App/ViewModel.js
@@ -20,6 +20,7 @@
         $.ajax({
             type: 'GET',
             url: 'http://localhost:51478/api/Game',
+            cache: false,
             contentType: 'application/json',
             success: function (data) {
                 var elements = [];
@@ -37,13 +38,23 @@
             $.ajax({
                 type: 'PUT',
                 url: 'http://localhost:51478/api/Game',
+                cache: false,
                 data: ko.toJSON({
                     player1: self.player1,
                     player2: self.player2
                 }),
                 contentType: 'application/json',
                 success: function (data) {
-                    self.winner(data.GameWinner.Name + ' with ' + data.GameWinner.ChosenElement);
+                    var player1 = data.GamePlayed.Player1;
+                    var player2 = data.GamePlayed.Player2;
+                    
+                    var p1 = self.elements()[player1.ChosenElement];
+                    var p2 = self.elements()[player2.ChosenElement];
+                    
+                    self.player1().chosenElement(p1);
+                    self.player2().chosenElement(p2);
+
+                    self.winner(data.GameWinner.Name + ' with ' + self.elements()[data.GameWinner.ChosenElement]);
                 },
                 error: function(error) {
                     alert('error: ' + error);
